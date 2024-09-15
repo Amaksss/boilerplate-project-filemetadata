381318a0f645be78b902ca1a1582abe7ca605000
Amaksss <amakaorabuchi@gmail.com>
2024-09-15 15:11:44 +0100

    file upload

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+
+
 
 var app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: true }));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI, {})
+
+const db = mongoose.connection
+
+//listen for error in connection process
+db.on('error', () => {
+  console.log('Connected to MongoDB');
+})
+
+//listen for a successful connection
+db.once('open', () => {
+  console.log('Connected to MongoDb')
+})
+
+;
+
+
+
+//initialize multer 
+const upload = multer ()
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+//andle form submission with the file upload
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  //check if file was uploaded
+  if(!req.file) {
+    res.status(400).json({ error: 'no file uploaded'})
+  }
+  res.status(200).json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
